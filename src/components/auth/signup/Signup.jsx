682f6d3454196900a2828c851721dd0998474383
mburrow303 <elio21@comcast.net>
2023-10-28 17:35:01 -0600

    Auth component

---
+++ src/components/auth/signup/Signup.jsx
@@ -0,0 +1,50 @@
+{ /* import React from 'react';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  ListGroup,
+  ListGroupItem,
+  CardLink
+} from 'reactstrap';
+
+<Card
+  style={{
+    width: '18rem'
+  }}
+>
+  <img
+    alt="Card"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      Card Title
+    </CardTitle>
+    <CardText>
+      This is some text within a card body.
+    </CardText>
+  </CardBody>
+  <ListGroup flush>
+    <ListGroupItem>
+      An item
+    </ListGroupItem>
+    <ListGroupItem>
+      A second item
+    </ListGroupItem>
+    <ListGroupItem>
+      And a third item
+    </ListGroupItem>
+  </ListGroup>
+  <CardBody>
+    <CardLink href="#">
+      Card Link
+    </CardLink>
+    <CardLink href="#">
+      Another Card Link
+    </CardLink>
+  </CardBody>
+</Card> */}
+
+//export default Signup;
